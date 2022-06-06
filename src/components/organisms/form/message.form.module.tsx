80c9f09874f838db3295ab725617008d5b74f0c3
Rabin Thapa <imrabin1998@gmail.com>
2022-06-06 23:59:47 +0545

    successfull connection

--- src/components/organisms/form/message.form.module.tsx
+++ src/components/organisms/form/message.form.module.tsx
@@ -1,39 +1,62 @@
-import React, { useId } from "react";
+import React, { useId, useEffect } from "react";
 import styles from "./message.form.module.css";
-import button from "../../atoms/button/button.module.css";
-import { io } from "socket.io-client";
+import { io, connect } from "socket.io-client";
 
 const MessageForm = () => {
-  const socket = io("http://localhost:8080");
   const id1 = useId();
   const id2 = useId();
+  const [message, setMessage] = React.useState("");
+  const [display, setDisplay] = React.useState("");
+
+  const socket = io("http://localhost:8080");
+
+  socket.on("connect", () => {
+    console.log("you are connected", socket.id);
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    socket.emit("send-message", message);
+    socket.on("receive-message", (message) => {
+      setDisplay(message);
+    });
   };
-  socket.on("connect", () => {
-    alert("You are connected with Id: " + socket.id);
-    socket.emit("custom-event", 10, "a", { b: "ball" });
-  });
+
   return (
     <>
       <h1 className={styles.headerTopTitle}>chat room</h1>
+      <div className={styles.chatContainer}>
+        <div className={styles.chatBox}>
+          <p>{display}</p>
+        </div>
 
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-2">
-            <label htmlFor={id1} className={` ${styles.headerTopTitle}`}>
-              Message
-            </label>
-            <input type="text" id={id1} />
-            <label htmlFor={id2} className={styles.headerTopTitle}>
-              Room
-            </label>
-            <input type="text" id={id2} />
-          </div>
-          <button type="submit" className={`mx-auto ${button.button}`}>
-            Send
-          </button>
-        </form>
+        <div className={styles.formContainer}>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-2">
+              <div className={styles.formGroup}>
+                <label htmlFor={id1} className={styles.headerTopTitle}>
+                  Message
+                </label>
+                <input
+                  type="text"
+                  id={id1}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor={id2} className={styles.headerTopTitle}>
+                  Room
+                </label>
+                <input type="text" id={id2} />
+              </div>
+            </div>
+            <div className={styles.btnContainer}>
+              <button type="submit" className={styles.button}>
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
